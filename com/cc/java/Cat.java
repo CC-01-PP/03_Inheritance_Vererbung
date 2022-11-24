a65ca3b77d1ed79c2e230709fa248c44765d856c
Mia Kamenetski <mia.kamenetski@online.de>
2022-11-24 13:26:20 +0100

    Tomcat

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -4,17 +4,13 @@ public class Cat {
  
    private String name;
    private String furColor;
-
    private int age;
-   private boolean isFemale;
-
    private int counter;
 
-    public Cat(String name, String furColor, int age, boolean isFemale) {
+    public Cat(String name, String furColor, int age) {
         this.name = name;
         this.furColor = furColor;
         this.age = age;
-        this.isFemale = isFemale;
 	}
 
     public String getStringAttributes(String flag) {
@@ -29,17 +25,11 @@ public class Cat {
     }
 
     public String getAge() {
-        if (!isFemale) {
-            return Integer.toString(age);
-        } else {
             return checkEscalationLevel(); 
         }
-    }
-
+    
     private String checkEscalationLevel() {
-    
-    counter++;
-    
+        counter++;
         switch (counter) {
             case 1:
                 return "This is an inappropriate question!";
